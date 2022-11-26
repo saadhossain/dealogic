ca8638f3cc5b069814621101feff2960345a4342
saadhossain <mshsarkar@gmail.com>
2022-11-27 00:30:54 +0600

    All Users shown on the admin route/ui

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -19,33 +19,35 @@ const Header = () => {
             })
     }
     return (
-        <div className='w-11/12 lg:w-10/12 mx-auto my-1 flex justify-between items-center'>
-            <Link to='/'><img src={logo} alt='Innova' className='w-28' /></Link>
-            <div className='flex items-center gap-2'>
-                <ul className={`md:flex items-center gap-3 font-semibold absolute md:static py-5 px-10 shadow-lg md:shadow-none ${expand ? 'top-8 right-0' : 'top-[-200px] right-0'}`}>
-                    <li className='hover:text-innova'><NavLink to='/'>Home</NavLink></li>
-                    <li className='hover:text-innova'><NavLink to='/blogs'>Blog</NavLink></li>
-                    <li className='hover:text-innova'><NavLink to='/dashboard/addProduct'>Sell</NavLink></li>
-                    {
-                        user?.email
-                            ? <>
-                                <Link to='/dashboard'>
-                                    <li className='hover:text-innova'>Dashboard</li>
-                                </Link>
-                                <Link to='/dashboard'>
-                                    <img src={user?.photoURL ? user.photoURL : ''} alt={user?.displayName} className='w-10 rounded-full' />
-                                </Link>
-                                <Link onClick={handleLogOut}>
-                                    <AiOutlineLogout className='w-6 h-6 text-accent'></AiOutlineLogout>
-                                </Link>
-                            </>
-                            : <li className='hover:text-innova'><NavLink to='/login'>Login</NavLink></li>
-                    }
-                </ul>
-                <div onClick={() => setExpand(!expand)} className='w-6 h-6 lg:hidden'>
-                    {
-                        expand ? <XMarkIcon></XMarkIcon> : <Bars3Icon></Bars3Icon>
-                    }
+        <div className='shadow-lg mb-5'>
+            <div className='w-11/12 lg:w-10/12 mx-auto my-1 flex justify-between items-center'>
+                <Link to='/'><img src={logo} alt='Innova' className='w-28' /></Link>
+                <div className='flex items-center gap-2'>
+                    <ul className={`md:flex items-center gap-3 font-semibold absolute md:static py-2 px-10 shadow-lg md:shadow-none ${expand ? 'top-8 right-0' : 'top-[-200px] right-0'}`}>
+                        <li className='hover:text-innova'><NavLink to='/'>Home</NavLink></li>
+                        <li className='hover:text-innova'><NavLink to='/blogs'>Blog</NavLink></li>
+                        <li className='hover:text-innova'><NavLink to='/dashboard/addProduct'>Sell</NavLink></li>
+                        {
+                            user?.email
+                                ? <>
+                                    <Link to='/dashboard'>
+                                        <li className='hover:text-innova'>Dashboard</li>
+                                    </Link>
+                                    <Link to='/dashboard'>
+                                        <img src={user?.photoURL ? user.photoURL : ''} alt={user?.displayName} className='w-10 rounded-full' />
+                                    </Link>
+                                    <Link onClick={handleLogOut}>
+                                        <AiOutlineLogout className='w-6 h-6 text-accent'></AiOutlineLogout>
+                                    </Link>
+                                </>
+                                : <li className='hover:text-innova'><NavLink to='/login'>Login</NavLink></li>
+                        }
+                    </ul>
+                    <div onClick={() => setExpand(!expand)} className='w-6 h-6 lg:hidden'>
+                        {
+                            expand ? <XMarkIcon></XMarkIcon> : <Bars3Icon></Bars3Icon>
+                        }
+                    </div>
                 </div>
             </div>
         </div>
--- src/Pages/AllUsers/AllUsers.js
+++ src/Pages/AllUsers/AllUsers.js
@@ -1,68 +1,33 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useContext } from 'react';
+import React from 'react';
 import toast from 'react-hot-toast';
 import { FaTrash } from 'react-icons/fa';
-import { AuthContext } from '../../Context/AuthProvider';
-import {RiRocket2Fill} from 'react-icons/ri'
 
 const AllUsers = () => {
-    //Get User from the Context
-    const { user } = useContext(AuthContext)
-    //Get Products for logged in users
-    const { data: myProducts = [], refetch } = useQuery({
-        queryKey: ['myProducts', user?.email],
-        queryFn: () => fetch(`http://localhost:5000/products?email=${user?.email}`)
-            .then(res => res.json())
+    //Get all Users from the database
+    const { data: allUsers = [], refetch } = useQuery({
+        queryKey: ['allUsers'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users')
+            const data = await res.json()
+            return data;
+        }
     })
-    //Set Product Status to the Database
-    const handleStatusChange = (id) => {
-        fetch(`http://localhost:5000/products/${id}`, {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({ prodStatus : 'Sold' })
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (data.modifiedCount > 0) {
-                    toast.success('Product Status Updated...')
-                    refetch()
-                }
-            })
-    }
-    //Remove Product from listing
-    const handleRemoveProduct = (id) => {
-        const confirmation = window.confirm('Do You Want to Delete This Item?')
+    //Remove or Delete a User
+    const handleDeleteUser = (id) => {
+        const confirmation = window.confirm('Do You Want to Delete This User?')
         if (confirmation) {
-            fetch(`http://localhost:5000/products/${id}`, {
+            fetch(`http://localhost:5000/users/${id}`, {
                 method: 'DELETE'
             })
                 .then(res => res.json())
                 .then(data => {
                     if(data.deletedCount > 0){
-                        toast.error('One Product has been Deleted...')
+                        toast.error('One User has been Deleted...')
                         refetch()
                     }
                 })
         }
-    }
-    //Promote Product by Seller
-    const handlePromote = (id) => {
-        fetch(`http://localhost:5000/products/${id}`, {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify({ promoted: true })
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (data.modifiedCount > 0) {
-                    toast.success('Product Boosted Successfully....')
-                    refetch()
-                }
-            })
     }
     return (
         <div>
@@ -76,43 +41,27 @@ const AllUsers = () => {
                         <thead>
                             <tr>
                                 <th></th>
-                                <th>Product Name</th>
-                                <th>Regular Price</th>
-                                <th>Sale Price</th>
-                                <th>Status</th>
-                                <th>Promote</th>
+                                <th>User Name</th>
+                                <th>Profile</th>
+                                <th>Email</th>
+                                <th>Role</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
                         <tbody className='font-semibold'>
                             {
-                                myProducts.map((myproduct, idx) => <tr
-                                    key={myproduct._id}
+                                allUsers.map((users, idx) => <tr
+                                    key={users._id}
                                 >
                                     <th>{idx + 1}</th>
-                                    <td>{myproduct.proName}</td>
-                                    <td>${myproduct.regularPrice}</td>
-                                    <td>${myproduct.resalePrice}</td>
-                                    <td className='flex items-center gap-1'>
-                                        <button
-                                        onClick={() => handleStatusChange(myproduct._id)}
-                                        className={`duration-300 py-1 px-2 rounded text-white font-semibold ${myproduct.prodStatus === 'Sold' ? 'bg-accent' : 'bg-innova hover:bg-secondary'}`}
-                                        disabled={myproduct.prodStatus === 'Sold'}
-                                        >
-                                        {myproduct.prodStatus === 'Sold'? 'Sold' : 'Mark Sold'}
-                                        </button>
-                                    </td>
+                                    <td>{users.fullName}</td>
                                     <td>
-                                        <button
-                                        onClick={()=> handlePromote(myproduct._id)}
-                                        className={`flex items-center duration-300 py-1 px-2 rounded text-white ${myproduct.promoted ? 'bg-accent' : 'bg-innova hover:bg-secondary'}`}
-                                        disabled={myproduct.promoted}
-                                        >
-                                        <RiRocket2Fill></RiRocket2Fill>
-                                        {myproduct.promoted ? 'Promoted' : 'Promote'}</button>
+                                        <img src={users.profileImage} alt={users.fullName} className='w-12 rounded-full'/>
                                     </td>
+                                    <td>{users.email}</td>
+                                    <td>{users.accountType}</td>
                                     <td>
-                                        <button onClick={() => handleRemoveProduct(myproduct._id)} className='text-innova hover:text-red-700 duration-300'><FaTrash></FaTrash></button>
+                                        <button onClick={() => handleDeleteUser(users._id)} className='text-innova hover:text-red-700 duration-300 flex items-center gap-1'><FaTrash></FaTrash> Delete</button>
                                     </td>
                                 </tr>)
                             }
