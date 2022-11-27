db0f739f8eb35b47136770ec55e001262594061c
saadhossain <mshsarkar@gmail.com>
2022-11-27 10:21:29 +0600

    Product Display UI Modified

--- src/Components/ProductsComponents/DisplayPromotedProducts.js
+++ src/Components/ProductsComponents/DisplayPromotedProducts.js
@@ -6,17 +6,20 @@ const DisplayPromotedProducts = ({ promoted, setAvailableProduct }) => {
     const { proName, productImageURL, productCondition, resalePrice, sellerName, addedOn, location, booked } = promoted;
     return (
         <div>
-            <div className="card card-compact w-full bg-base-100 shadow-lg hover:shadow-2xl cursor-pointer">
+            <div className="card card-compact w-full bg-base-100 shadow-lg hover:shadow-2xl cursor-pointer relative">
+            <p className='font-semibold text-white px-2 shadow-lg absolute top-2 left-2 bg-innova rounded'>Promoted</p>
                 <figure><img src={productImageURL} alt={proName} className='w-full h-48 rounded-t' /></figure>
-                <div className="card-body font-semibold">
-                    <h2 className="text-2xl font-semibold">{proName}</h2>
-                    <p className='font-semibold text-2xl text-innova'>Sale: ${resalePrice}</p>
+                <div className="card-body font-semibold relative">
+                    <p className='font-semibold text-2xl text-white px-2 shadow-lg absolute -top-10 right-2 bg-innova rounded'>${resalePrice}</p>
+                    <h2 className="text-xl font-semibold">{proName}</h2>
                     <div className='flex justify-between items-center'>
                         <p className='flex items-center gap-1'><FaMapMarkerAlt></FaMapMarkerAlt> {location}</p>
                         <p>Condition :{productCondition}</p>
                     </div>
+                    <hr className="border-1 border-gray-400"/>
                     <p>Posted On: {dateFormat(addedOn, "mmm dS, h:MM: TT")}</p>
                     <p>Sold By: {sellerName}</p>
+                    <hr className="border-1 border-gray-400"/>
                     <div className="card-actions justify-center">
                         <label
                             className='btn border-none duration-300 ease-in-out bg-innova hover:bg-secondary'
--- src/Components/ProductsComponents/ProductShowcase.js
+++ src/Components/ProductsComponents/ProductShowcase.js
@@ -3,26 +3,27 @@ import React from 'react';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 
 const ProductShowcase = ({ product, setAvailableProduct }) => {
-    const {proName, productImageURL, productCondition, purchaseYear, regularPrice, resalePrice, sellerName, usedYear, description, addedOn, location, booked } = product;
+    const { proName, productImageURL, productCondition, purchaseYear, regularPrice, resalePrice, sellerName, usedYear, addedOn, location, booked } = product;
     return (
         <div>
             <div className="card card-compact w-full bg-base-100 shadow-lg hover:shadow-2xl duration-500 hover:-mt-2 cursor-pointer">
                 <figure><img src={productImageURL} alt={proName} className='w-full h-48 rounded-t' /></figure>
                 <div className="card-body font-semibold">
-                    <h2 className="text-2xl font-semibold">{proName}</h2>
+                    <h2 className="text-xl font-semibold">{proName}</h2>
                     <div className='flex items-center justify-between'>
                         <p className='line-through text-gray-500'>Regular :${regularPrice}</p>
-                        <p className='font-semibold text-2xl text-innova'>Sale: ${resalePrice}</p>
+                        <p className='font-bold text-xl text-innova'>Sale: ${resalePrice}</p>
                     </div>
-                    <p>
-                        {description.length > 50 && description.slice(0, 50) + '...'}
-                    </p>
+                    <hr className="border-1 border-gray-400"/>
                     <div className='flex justify-between items-center'>
                         <p className='flex items-center gap-1'><FaMapMarkerAlt></FaMapMarkerAlt> {location}</p>
                         <p>Used: {usedYear}</p>
                     </div>
-                    <p>Condition :{productCondition}</p>
-                    <p>Purchased on: {purchaseYear}</p>
+                    <div className='flex justify-between items-center'>
+                        <p>Status :{productCondition}</p>
+                        <p>Purchase: {purchaseYear}</p>
+                    </div>
+                    <hr className="border-1 border-gray-400"/>
                     <p>Posted On: {dateFormat(addedOn, "mmm dS, h:MM: TT")}</p>
                     <p>Sold By: {sellerName}</p>
                     <div className="card-actions justify-center">
@@ -30,7 +31,7 @@ const ProductShowcase = ({ product, setAvailableProduct }) => {
                             className='btn border-none duration-300 ease-in-out bg-innova hover:bg-secondary'
                             htmlFor="booking-modal"
                             disabled={booked}
-                            onClick={()=> setAvailableProduct(product)}
+                            onClick={() => setAvailableProduct(product)}
                         >{booked ? 'Already Booked' : 'Book Product'}</label>
                     </div>
                 </div>
