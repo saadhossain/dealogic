63be0b6026acf0cc8f329bc3b443fbcdaf419797
saadhossain <mshsarkar@gmail.com>
2022-12-08 18:07:03 +0600

    Safety section responsive issue fixed

--- src/Components/HomeSafety/HomeSafety.js
+++ src/Components/HomeSafety/HomeSafety.js
@@ -3,16 +3,16 @@ import safety from '../../assests/transaction.jpg';
 
 const HomeSafety = () => {
     return (
-        <div className='w-11/12 lg:w-10/12 mx-auto flex gap-2 items-center'>
-            <div className='w-2/4'>
-                <h1 className='text-5xl font-bold leading-[70px] tracking-[15px]'>
-                    <span className='text-dealogic text-7xl'>Be Safe</span> <br />
+        <div className='w-11/12 lg:w-10/12 mx-auto lg:flex gap-2 items-center my-10'>
+            <div className='w-11/12 lg:w-2/4'>
+                <h1 className='text-3xl lg:text-5xl font-bold leading-[70px] tracking-[10px] lg:tracking-[15px]'>
+                    <span className='text-dealogic text-5xl lg:text-7xl'>Be Safe</span> <br />
                     While Making <br />
                     TRANSACTION
                 </h1>
             </div>
-            <div className='w-2/4'>
-                <img src={safety} alt='Safety Banner' className='rounded-lg shadow-xl'/>
+            <div className='w-11/12 lg:w-2/4 mt-5 lg:mt-0'>
+                <img src={safety} alt='Safety Banner' className='rounded-lg shadow-xl' />
             </div>
         </div>
     );
