af96cfe16ddf0e549075dd9223acf1b07933c581
saadhossain <mshsarkar@gmail.com>
2022-12-08 21:14:25 +0600

    Loading Added On Blogs page

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,11 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import DisplayBlogs from '../../Components/BlogsComp/DisplayBlogs';
+import Loader from '../../Components/Spinners/Loader';
 
 const Blogs = () => {
 
     //Get All Blogs
-    const { data: blogs = [] } = useQuery({
+    const { data: blogs = [], isLoading } = useQuery({
         queryKey: ['blogs'],
         queryFn: async () => {
             const res = await fetch('https://dealogic.vercel.app/blogs');
@@ -13,6 +14,9 @@ const Blogs = () => {
             return data
         }
     })
+    if(isLoading){
+        return <Loader></Loader>
+    }
     return (
         <div className='w-11/12 lg:w-10/12 mx-auto my-5'>
             <div className='relative'>
