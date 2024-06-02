acb2c0362f28fd4e5b9bddf96610a5033c05d438
saadhossain <mshsarkar@gmail.com>
2024-06-02 21:42:11 +0600

    Banner changed

--- web/src/Components/HomeComponents/HomeBanner.js
+++ web/src/Components/HomeComponents/HomeBanner.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
-import Banner1 from '../../assests/banners/20Off-Banner.jpg';
-import Banner2 from '../../assests/banners/Computer-Accessories.jpg'
-import Banner3 from '../../assests/banners/Computer-Gadgets.jpg'
+import Banner1 from '../../assests/banners/banner-1.png';
+import Banner2 from '../../assests/banners/banner-2.png'
+import Banner3 from '../../assests/banners/banner-3.png'
 import './HomeBanner.css'
 
 const HomeBanner = () => {
@@ -18,7 +18,7 @@ const HomeBanner = () => {
         arrows: true
     };
     return (
-        <div>
+        <div className='w-11/12 md:w-10/12 mx-auto mt-5'>
             <Slider {...settings}>
                 <div><img src={Banner1} alt='Home Banner' className='w-full'/></div>
                 <div><img src={Banner2} alt='Home Banner' className='w-full'/></div>
